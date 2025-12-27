8c61455bb672ca16dff2c6892ca0ec7ca5ef29c3
viniciusmecosta <costavinicius315@gmail.com>
2025-12-27 15:46:14 -0300

    refactor: make db engine args conditional for postgres compatibility

--- app/database/session.py
+++ app/database/session.py
@@ -3,7 +3,12 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
+connect_args = {}
+if settings.SQLALCHEMY_DATABASE_URI.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
+    settings.SQLALCHEMY_DATABASE_URI,
+    connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
