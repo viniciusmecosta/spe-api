8c2be176013e0c1de044ec092e7e3c437331b261
viniciusmecosta <costavinicius315@gmail.com>
2026-02-25 10:16:08 -0300

    fix: corrigir localtime no backuo

--- app/services/backup_service.py
+++ app/services/backup_service.py
@@ -48,14 +48,14 @@ class BackupService:
             yesterday = yesterday_dt.date()
             formatted_date = yesterday.strftime("%d/%m/%Y")
 
-            start_utc = yesterday_dt.replace(hour=0, minute=0, second=0, microsecond=0).astimezone(pytz.utc)
-            end_utc = yesterday_dt.replace(hour=23, minute=59, second=59, microsecond=999999).astimezone(pytz.utc)
+            start_local = datetime.combine(yesterday, datetime.min.time())
+            end_local = datetime.combine(yesterday, datetime.max.time())
 
             records = (
                 db.query(TimeRecord, User)
                 .join(User, TimeRecord.user_id == User.id)
-                .filter(TimeRecord.record_datetime >= start_utc)
-                .filter(TimeRecord.record_datetime <= end_utc)
+                .filter(TimeRecord.record_datetime >= start_local)
+                .filter(TimeRecord.record_datetime <= end_local)
                 .order_by(User.name, TimeRecord.record_datetime)
                 .all()
             )
@@ -71,9 +71,8 @@ class BackupService:
                     user_activity[user.name] = []
 
                 local_time = record.record_datetime
-                if local_time.tzinfo is None:
-                    local_time = pytz.utc.localize(local_time)
-                local_time = local_time.astimezone(tz)
+                if local_time.tzinfo is not None:
+                    local_time = local_time.astimezone(tz)
 
                 time_str = local_time.strftime("%H:%M")
                 type_label = "E" if record.record_type == RecordType.ENTRY else "S"
