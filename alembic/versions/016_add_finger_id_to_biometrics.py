c3b264cfbd177d22f7f76070d2b2cea8465bba81
viniciusmecosta <costavinicius315@gmail.com>
2026-04-14 08:35:48 -0300

    feat: incluir finger_id na biometria

---
+++ alembic/versions/016_add_finger_id_to_biometrics.py
@@ -0,0 +1,15 @@
+import sqlalchemy as sa
+from alembic import op
+
+revision = '016'
+down_revision = '015'
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    with op.batch_alter_table('user_biometrics', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('finger_id', sa.Integer(), nullable=True))
+
+def downgrade() -> None:
+    with op.batch_alter_table('user_biometrics', schema=None) as batch_op:
+        batch_op.drop_column('finger_id')
