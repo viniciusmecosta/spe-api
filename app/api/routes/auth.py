500ab568b710636f469e952ab7fd7d51051a12e6
viniciusmecosta <costavinicius315@gmail.com>
2026-04-16 08:19:16 -0300

    fix: corrigir endpoint de login com retorno 500

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -19,18 +19,17 @@ def login_access_token(db: Session = Depends(deps.get_db), form_data: OAuth2Pass
     username = form_data.username.lower()
     user = user_repository.get_by_username(db, username=username)
 
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+
     is_dev = settings.ENVIRONMENT.lower() == "dev"
 
     if not is_dev:
-        if not user or not security.verify_password(form_data.password, user.password_hash):
+        if not security.verify_password(form_data.password, user.password_hash):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
-        if not user.is_active:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
-    else:
-        if not user:
-            if not user:
-                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                    detail="No users found in database for DEV bypass")
+
+    if not user.is_active:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
 
     access_token = security.create_access_token(subject=user.id)
     return {"access_token": access_token, "token_type": "bearer"}
